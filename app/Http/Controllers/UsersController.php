f7ef46b993ffa324dc206b8303f7048119468c28
Kevin <kevinihm@gmail.com>
2020-02-29 19:06:55 -0800

    added a login test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Users;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -13,7 +14,9 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        Users::all();
+
+        return view('');
     }
 
     /**
